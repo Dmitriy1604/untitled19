16a4fa3721d15a535796ec47a832108868688747
Dmitriy1604 <Dimon312-91@mail.ru>
2022-07-22 19:06:12 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,14 @@ public class Main {
 
     public static void task4() {
         int[] weight1 = {1,2,3,4,5};
-        for (int i = 0; i < weight1.length; i++) {
-            if (weight1[i] % 2 == 0) {
-                System.out.print(weight1[i] + ",");}
-            else {
+        for (int i = 0; i < weight1.length; i++)
+            if (weight1[i] % 2 == 0)
+                System.out.print(weight1[i] + ",");
+            else
                 System.out.print(weight1[i]+1 + ",");
 
 
-            }}}
+            }
 
 
     public static void task1() {
@@ -45,23 +45,16 @@ public class Main {
         int[] weight = {1, 2, 3};
         for (int i = 0; i < weight.length; i++)
             System.out.print(weight[i] + ",");
-        {
-
-        }
         System.out.println();
-
         double[] sum = {1.57, 7.654, 9.986};
         for (int i = 0; i < sum.length; i++)
             System.out.print(sum[i] + ",");
-        {
             System.out.println();
-        }
-        int[] weight1 = {90, 91, 93, 92, 85, 87, 84, 83};
+            int[] weight1 = {90, 91, 93, 92, 85, 87, 84, 83};
         for (int i = 0; i < weight1.length; i++)
             System.out.print(weight1[i] + ",");
-        {
             System.out.println();
-        }
+
     }
 
     public static void task3(){
@@ -69,23 +62,19 @@ public class Main {
         for (int i = weight.length - 1; i > -1; i--) {
             System.out.print(weight[i]);
             if (i > 0)
-                System.out.print(",");
-        }
+                System.out.print(",");}
         System.out.println();
         double[] mass = {1.57, 7.654, 9.986};
         for (int i = mass.length - 1; i > -1; i--) {
             System.out.print(mass[i]);
             if (i > 0)
-                System.out.print(",");
-        }
+                System.out.print(",");}
         System.out.println();
         int[] weight1 = {90, 91, 93, 92, 85, 87, 84, 83};
         for (int i = weight1.length - 1; i > -1; i--) {
             System.out.print(weight1[i]);
             if (i > 0)
-                System.out.print(",");
-
-        }
+                System.out.print(",");}
         System.out.println();
     }
 }
